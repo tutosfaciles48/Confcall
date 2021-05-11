3fdc459a72b3bbafa05ef6aebb699064aa381f73
Romain Neil <tutosfaciles48@gmail.com>
2021-05-11 22:15:04 +0200

    refactor(LdapFormAuthenticator): reformat file and add return type to functions

--- src/Security/LdapFormAuthenticator.php
+++ src/Security/LdapFormAuthenticator.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -18,85 +17,79 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LdapFormAuthenticator extends AbstractFormLoginAuthenticator
-{
-    use TargetPathTrait;
+class LdapFormAuthenticator extends AbstractFormLoginAuthenticator {
 
-    public const LOGIN_ROUTE = 'app_login';
+	use TargetPathTrait;
+
+	public const LOGIN_ROUTE = 'app_login';
 
 	private UrlGeneratorInterface $urlGenerator;
 	private CsrfTokenManagerInterface $csrfTokenManager;
 	protected Ldap $ldap;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, Ldap $ldap)
-    {
-        $this->urlGenerator = $urlGenerator;
-        $this->csrfTokenManager = $csrfTokenManager;
-        $this->ldap = $ldap;
-    }
+	public function __construct(UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, Ldap $ldap) {
+		$this->urlGenerator = $urlGenerator;
+		$this->csrfTokenManager = $csrfTokenManager;
+		$this->ldap = $ldap;
+	}
 
-    public function supports(Request $request)
-    {
-        return self::LOGIN_ROUTE === $request->attributes->get('_route')
-            && $request->isMethod('POST');
-    }
+	public function supports(Request $request): bool {
+		return self::LOGIN_ROUTE === $request->attributes->get('_route')
+			&& $request->isMethod('POST');
+	}
 
-    public function getCredentials(Request $request)
-    {
-        $credentials = [
-            'username' => $request->request->get('username'),
-            'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
-        ];
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['username']
-        );
+	public function getCredentials(Request $request): array {
+		$credentials = [
+			'username' => $request->request->get('username'),
+			'password' => $request->request->get('password'),
+			'csrf_token' => $request->request->get('_csrf_token'),
+		];
+		$request->getSession()->set(
+			Security::LAST_USERNAME,
+			$credentials['username']
+		);
 
-        return $credentials;
-    }
+		return $credentials;
+	}
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
+	public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface {
+		$token = new CsrfToken('authenticate', $credentials['csrf_token']);
+		if (!$this->csrfTokenManager->isTokenValid($token)) {
+			throw new InvalidCsrfTokenException();
+		}
 
-        // Load / create our user however you need.
-        // You can do this by calling the user provider, or with custom logic here.
-        $user = $userProvider->loadUserByUsername($credentials['username']);
+		// Load / create our user however you need.
+		// You can do this by calling the user provider, or with custom logic here.
+		$user = $userProvider->loadUserByUsername($credentials['username']);
 
-        if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
-        }
+		if (!$user) {
+			// fail authentication with a custom error
+			throw new CustomUserMessageAuthenticationException('Username could not be found.');
+		}
 
-        return $user;
-    }
+		return $user;
+	}
 
-    public function checkCredentials($credentials, UserInterface $user)
-    {
-        try {
-        	$this->ldap->bind($user->getEntry()->getDn(), $credentials['password']);
+	public function checkCredentials($credentials, UserInterface $user): bool {
+		try {
+			$this->ldap->bind($user->getEntry()->getDn(), $credentials['password']);
 		} catch (ConnectionException $e) {
-        	return false;
+			return false;
 		}
 
 		return true;
-    }
+	}
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
-        }
+	public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): RedirectResponse {
+		if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+			return new RedirectResponse($targetPath);
+		}
 
-        return new RedirectResponse($this->urlGenerator->generate("app_index"));
-    }
+		return new RedirectResponse($this->urlGenerator->generate("app_index"));
+	}
 
-    protected function getLoginUrl()
-    {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-    }
+	protected function getLoginUrl(): string {
+		return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+	}
+
 }
